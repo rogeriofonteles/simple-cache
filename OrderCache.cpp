0f68a9f7e551a1d7fe6fa3658f6e34dbdad177cd
Rogerio Fonteles <rogeriofonteles@gmail.com>
2024-01-25 15:49:22 -0300

    first commit

---
+++ OrderCache.cpp
@@ -0,0 +1,26 @@
+// Your implementation of the OrderCache...
+#include "OrderCache.h"
+
+void OrderCache::addOrder(Order order) {
+  // Todo...
+}
+
+void OrderCache::cancelOrder(const std::string& orderId) {
+  // Todo...
+}
+
+void OrderCache::cancelOrdersForUser(const std::string& user) {
+  // Todo...
+}
+
+void OrderCache::cancelOrdersForSecIdWithMinimumQty(const std::string& securityId, unsigned int minQty) {
+  // Todo...
+}
+
+unsigned int OrderCache::getMatchingSizeForSecurity(const std::string& securityId) {
+  // Todo...
+}
+
+std::vector<Order> OrderCache::getAllOrders() const {
+  // Todo...
+}
